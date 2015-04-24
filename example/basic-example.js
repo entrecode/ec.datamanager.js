34065ec800f501f47efdb72f582194f0107f2256
Ruben Deyhle <deyhle@entrecode.de>
2015-04-24 09:26:38 +0200

    callback-free-all-promise waitUntilDataManagerIsReady function

--- example/basic-example.js
+++ example/basic-example.js
@@ -7,7 +7,7 @@ var DataManager = require('../index.js'); // in your own project, just use requi
 
 var dataManager = new DataManager({
   url: 'https://datamanager.entrecode.de/api/f84710b8/',
-//  accessToken: 'e63dca99-6a56-43a5-8864-1a63ee8565e7'
+  accessToken: 'e63dca99-6a56-43a5-8864-1a63ee8565e7'
 });
 
 dataManager.modelList().then(function(modelList) {
