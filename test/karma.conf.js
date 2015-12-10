24e8aa38818212591085ceda84eedc8f938f1b20
Simon Scherzinger <scherzinger@entrecode.de>
2015-12-10 10:05:14 +0100

    added compatibility for phantomjs2

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -50,10 +50,10 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    //browsers: ['Chrome'],
     //browsers: ['Firefox'], // install launcher
     //browsers: ['Safari'],  // install launcher
-    //browsers: ['PhantomJS'],  // install launcher
+    browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
