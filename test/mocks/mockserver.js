aabac1b6bce6cf6ba68654689a295ef9785fc561
Simon Scherzinger <scherzinger@entrecode.de>
2016-02-17 15:39:30 +0100

    lodashs isEqual is needed in frontend mocks... stupid me.

--- test/mocks/mockserver.js
+++ test/mocks/mockserver.js
@@ -8,12 +8,13 @@
 'use strict';
 
 var bodyParser = require('body-parser')
-  , express    = require('express')
-  , fs         = require('fs')
-  , path       = require('path')
-  , walk       = require('walk')
-  , _          = require('lodash')
-  ;
+  , express = require('express')
+  , fs = require('fs')
+  , path = require('path')
+  , walk = require('walk')
+  , _ = {};
+
+_.isEqual = require('lodash.isequal');
 
 var port = 54815;
 
@@ -34,8 +35,8 @@ app.all('/*', function(req, res, next) {
     return res.status(200).send('GET, PUT, POST, DELETE').end();
   }
   var filePath = path.resolve(__dirname, req.path.slice(1));
-  var walker   = walk.walk(filePath);
-  var stop     = false;
+  var walker = walk.walk(filePath);
+  var stop = false;
   walker.on('file', function(root, fileStat, nextFile) {
     if (stop) {
       return nextFile();
@@ -58,7 +59,7 @@ app.all('/*', function(req, res, next) {
       if (!_.isEqual(file.req, req.body)) {
         return nextFile();
       }
-      stop     = true;
+      stop = true;
       file.res = JSON.parse(JSON.stringify(file.res)
       .split('https://datamanager.entrecode.de').join('http://localhost:54815/datamanager')
       .split('https://appserver.entrecode.de').join('http://localhost:54815/appserver')
